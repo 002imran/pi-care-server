3a2dbdc12561ea25d0a5aa526e7ec9f6b7e0d18b
alimranhossain <alimranhossain44@gmail.com>
2022-10-25 17:33:51 +0600

    rquire data and send to clinet side

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) =>{
     res.send('PICare Server is Running');
+});
+
+app.get('/course-list', (req, res) =>{
+    res.send(courses);
 })
+
+
 
 app.listen(port, ()=>{
     console.log('PICare server is running on port:', port);
